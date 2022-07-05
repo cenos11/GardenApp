cb869c73b7f6f7462418afdc4c437e4ad03e237d
cenos11 <cenos11@gmail.com>
2022-07-04 21:24:43 -0500

    playing with PySimpleGUI

--- garden_gui.py
+++ garden_gui.py
@@ -1,3 +1,16 @@
 import PySimpleGUI as sg
 
-sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
+layout = [[sg.Text("Gardenia App")], [sg.Button("Make me a calendar!")]]
+
+# Create the window
+window = sg.Window("Gardenia", layout, margins = (250, 150))
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
